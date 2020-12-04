d1efc00ae421dad05ddc4feb5663fcbd782b49c4
= <ArbazAhmed.cs19@bmsce.ac.in>
2020-12-04 16:03:26 +0530

    cpp

--- c++ excersise/fibbonacci.cpp
+++ c++ excersise/fibbonacci.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void fibnonacci(int n) {
-	int n1=0,n2=1,n3;
+void fibnonacci(int n)
+{
+	int n1 = 0, n2 = 1, n3;
 	cout << n1 << " " << n2 << " ";
 
-	for(int i=2; i<n; i++) {
+	for (int i = 2; i < n; i++)
+	{
 		n3 = n1 + n2;
 		cout << n3 << " ";
-		n1=n2;
-		n2=n3;
+		n1 = n2;
+		n2 = n3;
 	}
 }
-int main() {
+int main()
+{
 
 	int n;
 	cout << "Enter the value of N: \n";
@@ -20,4 +23,3 @@ int main() {
 	fibnonacci(n);
 	return 0;
 }
-
